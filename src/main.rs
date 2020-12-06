46af588f7c5dcae3d0311b30a634c654cabf154b
Arne Bahlo <hallo@arne.me>
2020-12-06 17:29:02 +0100

    Add basic ping/pong functionality

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,46 @@
-extern crate dotenv;
-extern crate env_logger;
-extern crate log;
-
 use dotenv::dotenv;
 use log::debug;
+use serenity::async_trait;
+use serenity::client::{Client, Context, EventHandler};
+use serenity::framework::standard::{
+    macros::{command, group},
+    CommandResult, StandardFramework,
+};
+use serenity::model::channel::Message;
 use std::env;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[group]
+#[commands(ping)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv().ok();
     env_logger::init();
 
     let discord_token = env::var("DISCORD_TOKEN")?;
 
-    debug!("discord_token: {}", discord_token);
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .group(&GENERAL_GROUP);
+
+    let mut client = Client::builder(discord_token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await?;
+
+    // start listening for events by starting a single shard
+    client.start().await?;
+    Ok(())
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
     Ok(())
 }
