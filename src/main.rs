068b188b7b4359b70f33d4b1b2b1bce5443fa4e1
Arne Bahlo <hallo@arne.me>
2020-12-07 14:06:35 +0100

    Split logic into fns

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use dotenv::dotenv;
-use log::info;
+use log::{error, info};
 use serenity::http::client::Http;
 use serenity::http::GuildPagination;
+use serenity::model::channel::GuildChannel;
+use serenity::model::guild::GuildInfo;
 use serenity::model::id::GuildId;
 use std::env;
 
@@ -11,23 +13,42 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
 
     let discord_token = env::var("DISCORD_TOKEN")?;
-    let http_client = Http::new_with_token(&discord_token);
+    let client = Http::new_with_token(&discord_token);
 
-    let guilds = http_client
+    let guilds = client
         .get_guilds(&GuildPagination::After(GuildId(0)), 1)
         .await?;
-    let first_guild = guilds.get(0).expect("No guild provided"); // TODO: Support multiple guilds
-    info!("guild: {:?}", first_guild);
 
-    let channels = http_client.get_channels(*first_guild.id.as_u64()).await?;
-    info!("channels: {:?}", channels);
-
-    for channel in channels {
-        let messages = http_client
-            .get_messages(*channel.id.as_u64(), "?limit=100")
-            .await?;
-        info!("got {} messages in {}", messages.len(), channel.name);
+    for guild in guilds {
+        info!("Processing guild {}", guild.name);
+        if let Err(e) = process_guild(&client, &guild).await {
+            error!("Could not process guild {}: {:?}", guild.name, e)
+        }
     }
 
+    Ok(())
+}
+
+async fn process_guild(client: &Http, guild: &GuildInfo) -> Result<(), Box<dyn std::error::Error>> {
+    let channels = client.get_channels(*guild.id.as_u64()).await?;
+    for channel in channels {
+        if let Err(e) = process_channel(client, &channel).await {
+            error!(
+                "Could not process channel {} in guild {}: {:?}",
+                channel.name, guild.name, e
+            )
+        }
+    }
+    Ok(())
+}
+
+async fn process_channel(
+    client: &Http,
+    channel: &GuildChannel,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let messages = client
+        .get_messages(*channel.id.as_u64(), "?limit=100")
+        .await?;
+    info!("got {} messages in {}", messages.len(), channel.name);
     Ok(())
 }
