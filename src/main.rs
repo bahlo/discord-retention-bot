aaea3963f050637d3f0e2b95c562ee774b11ba8d
Arne Bahlo <hallo@arne.me>
2020-12-07 14:41:40 +0100

    Implement filter_messages

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,6 @@ use serenity::model::channel::{GuildChannel, Message};
 use serenity::model::guild::GuildInfo;
 use serenity::model::id::GuildId;
 use std::env;
-use std::ops::Sub;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -18,8 +17,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let discord_token = env::var("DISCORD_TOKEN")?;
     let client = Http::new_with_token(&discord_token);
 
-    // TODO: Make timerange configurable
-    let two_weeks_ago = Utc::now().sub(Duration::weeks(2));
+    // TODO: Make configurable
+    let max_age = Duration::weeks(2);
 
     let guilds = client
         .get_guilds(&GuildPagination::After(GuildId(0)), 1)
@@ -27,7 +26,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     for guild in guilds {
         info!("Processing guild {}", guild.name);
-        if let Err(e) = process_guild(&client, &guild, two_weeks_ago).await {
+        if let Err(e) = process_guild(&client, &guild, max_age).await {
             error!("Could not process guild {}: {:?}", guild.name, e)
         }
     }
@@ -38,11 +37,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 async fn process_guild(
     client: &Http,
     guild: &GuildInfo,
-    delete_before: DateTime<Utc>,
+    max_age: Duration,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let channels = client.get_channels(*guild.id.as_u64()).await?;
     for channel in channels {
-        if let Err(e) = process_channel(client, &channel, delete_before).await {
+        if let Err(e) = process_channel(client, &channel, max_age).await {
             error!(
                 "Could not process channel {} in guild {}: {:?}",
                 channel.name, guild.name, e
@@ -55,13 +54,13 @@ async fn process_guild(
 async fn process_channel(
     client: &Http,
     channel: &GuildChannel,
-    delete_before: DateTime<Utc>,
+    max_age: Duration,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let first_batch = client
         .get_messages(*channel.id.as_u64(), "?limit=100")
         .await?;
 
-    let message_ids_to_delete: Vec<u64> = filter_messages(first_batch, delete_before)?;
+    let message_ids_to_delete: Vec<u64> = filter_messages(first_batch, max_age);
 
     // TODO: Implement pagination loop to get all messages, append to message_ids_to_delet
 
@@ -71,10 +70,11 @@ async fn process_channel(
     Ok(())
 }
 
-fn filter_messages(
-    _messages: Vec<Message>,
-    _before: DateTime<Utc>,
-) -> Result<Vec<u64>, Box<dyn std::error::Error>> {
-    // TODO: To filtering
-    Ok(vec![])
+fn filter_messages(messages: Vec<Message>, max_age: Duration) -> Vec<u64> {
+    let now = Utc::now();
+    messages
+        .into_iter()
+        .filter(|msg| return now.signed_duration_since(msg.timestamp) > max_age)
+        .map(|msg| *msg.id.as_u64())
+        .collect()
 }
