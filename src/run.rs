d32a954b46d4a0af7fdc9739d854d0ba0996e96a
Arne Bahlo <hallo@arne.me>
2020-12-12 15:00:00 +0100

    Add first integration test

--- src/run.rs
+++ src/run.rs
@@ -162,3 +162,90 @@ async fn delete_messages(
     }
     Ok(message_ids.len() as u64)
 }
+
+#[cfg(test)]
+mod tests {
+    use chrono::Duration;
+    use dotenv::dotenv;
+    use rand::Rng;
+    use serenity::{
+        client::{validate_token, Client},
+        model::id::MessageId,
+        utils::MessageBuilder,
+    };
+    use std::env;
+    use tokio::runtime::Runtime;
+
+    use super::*;
+
+    const CHANNEL_NAME_CHARSET: &[u8] = b"abcdefghijklmnopqrstuvwxyz0123456789";
+
+    #[test]
+    fn test_integration_simple() {
+        dotenv().ok();
+        let discord_token =
+            env::var("INTEGRATION_DISCORD_TOKEN").expect("INTEGRATION_DISCORD_TOKEN is unset");
+        validate_token(&discord_token).expect("Token is invalid");
+
+        Runtime::new().unwrap().block_on(async {
+            let client = Client::builder(&discord_token).await.unwrap();
+            let http_client = &client.cache_and_http.http;
+
+            // Setup
+            let guilds = http_client
+                .get_guilds(&GuildPagination::After(GuildId(0)), 1)
+                .await
+                .expect("Could not get guilds");
+            let guild = guilds.first().expect("Need at least one guild");
+
+            let mut rng = rand::thread_rng();
+            let channel_name: String = (0..16)
+                .map(|_| {
+                    let idx = rng.gen_range(0, CHANNEL_NAME_CHARSET.len());
+                    CHANNEL_NAME_CHARSET[idx] as char
+                })
+                .collect();
+            let channel = guild
+                .id
+                .create_channel(&http_client, |c| {
+                    c.name(&channel_name).kind(ChannelType::Text)
+                })
+                .await
+                .unwrap();
+
+            // Add two messages and pin the latter
+            channel
+                .id
+                .say(&http_client, MessageBuilder::new().push("foo").build())
+                .await
+                .unwrap();
+            let bar_msg = channel
+                .id
+                .say(&http_client, MessageBuilder::new().push("bar").build())
+                .await
+                .unwrap();
+            bar_msg.pin(&client.cache_and_http).await.unwrap();
+
+            // Process channel
+            let mut channel_retention = HashMap::new();
+            channel_retention.insert(channel_name, Duration::seconds(0));
+            run(&http_client, &channel_retention, false).await.unwrap();
+
+            // Assert we only have one message (the pinned one)
+            let messages = channel
+                .id
+                .messages(&http_client, |retriever| {
+                    retriever.after(MessageId(0)).limit(2)
+                })
+                .await
+                .unwrap();
+            assert_eq!(1, messages.len());
+
+            // Teardown
+            http_client
+                .delete_channel(*channel.id.as_u64())
+                .await
+                .expect("Could not delete channel");
+        });
+    }
+}
